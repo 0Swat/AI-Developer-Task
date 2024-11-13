4deab117cfe26ccfecfeb1329c4f8bb0eee794c3
Oskar Swat <oskar.swat@tlen.pl>
2024-11-13 12:44:50 +0100

    txt load

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+def load_article(filename="article.txt"):
+    try:
+        with open(filename, 'r') as f:
+            content = f.read()
+        return content
+    except FileNotFoundError:
+        print(f"Błąd: Plik '{filename}' nie został znaleziony.")
+        return None
+
+
 def main():
-    print("Init")
+    article_text = load_article()
 
 if __name__ == '__main__':
     main()
